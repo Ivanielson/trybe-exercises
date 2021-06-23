ae2a550ffa1afba6209992a1551665d7f62c294e
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-06-23 18:51:18 -0300

    Resolução exercício bloco 4.1 Exercício 1.1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise-bloco4-1.js
@@ -0,0 +1,16 @@
+// Adição (a + b)
+let a = 2;
+let b = 5;
+console.log(a + b);
+
+// Subtração (a - b)
+console.log(a - b);
+
+// Multiplicação (a * b)
+console.log(a * b);
+
+// Divisão (a / b)
+console.log(a / b);
+
+// Módulo (a % b)
+console.log(a % b);
