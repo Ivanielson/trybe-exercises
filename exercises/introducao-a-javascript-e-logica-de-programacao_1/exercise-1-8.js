d9a39414d7bfb2b78a6de2419167dc66d82a9713
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-06-24 01:35:19 -0300

    Resolução exercício bloco 4.1 Exercício 1.8

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise-1-8.js
@@ -0,0 +1,12 @@
+let number1 = 1;
+let number2 = 3;
+let number3 = 5;
+let test1 = number1 % 2;
+let test2 = number2 % 2;
+let test3 = number3 % 2;
+
+if (test1 === 0 || test2 === 0 || test3 === 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
