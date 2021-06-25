6a0994705a1bde2be7bee3e03e6ebcee034485a2
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-06-25 16:01:21 -0300

    Resolução exercício bloco 4.3 Exercício 1.2

---
+++ exercises/bloco_4/4-3/exercise1-2.js
@@ -0,0 +1,14 @@
+let n = 10;
+let simbolo = "*"
+
+for (let index = 1; index <= n; index+= 1) {
+  console.log(simbolo);
+  simbolo = simbolo + "*";
+}
+
+
+// console.log("*");
+// console.log("**");
+// console.log("***");
+// console.log("****");
+// console.log("*****");
