0aa9390c2a74417a7a8470710bb3481a3f24cf2d
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-06-28 18:28:00 -0300

    Resolução exercício bloco 4.4 Exercício 1.4

--- exercises/bloco_4/4-4/exercise1.js
+++ exercises/bloco_4/4-4/exercise1.js
@@ -1,12 +1,12 @@
-// Resolução exercício bloco 4.4 Exercício 1.3
+// Resolução exercício bloco 4.4 Exercício 1.4
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-info['recorrente'] = true;
+info['recorrente'] = 'sim';
 
 for (let key in info) {
-  console.log(key);
+  console.log(info[key]);
 }
