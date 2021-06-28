7c8bb22d9ea144c3e1e5155cc3bf63d24a01a6bb
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-06-28 18:38:21 -0300

    Resolução exercício bloco 4.4 Exercício 1.5

--- exercises/bloco_4/4-4/exercise1.js
+++ exercises/bloco_4/4-4/exercise1.js
@@ -1,12 +1,22 @@
-// Resolução exercício bloco 4.4 Exercício 1.4
+// Resolução exercício bloco 4.4 Exercício 1.5
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'sim'
 };
 
-info['recorrente'] = 'sim';
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'sim'
+};
 
 for (let key in info) {
-  console.log(info[key]);
+  if (info[key] === info2[key]) {
+    console.log('Ambos recorrentes');
+    continue;
+  }
+  console.log(info[key] + " e " + info2[key])
 }
