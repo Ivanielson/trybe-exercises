2641b33d4e4f847071f05f675101ea3a28f50ee4
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-07-04 01:18:13 -0300

    Resolução do exercício #3 do bloco_5.1

--- exercises/bloco_5/5.1/script.js
+++ exercises/bloco_5/5.1/script.js
@@ -6,9 +6,16 @@ function changeColorBackground(color) {
   document.getElementsByClassName('main-content')[0].style.backgroundColor = color;
 }
 
+function changeColorBackground2(color) {
+  document.getElementsByClassName('center-content')[0].style.backgroundColor = color;
+}
+
 let text = "Eu me vejo daqui a 2 anos trabalhando em um grande empresa na area de tecnologia, fazendo o que eu gosto, desenvolvendo sistemas para web.";
 let color = 'rgb(76,164,109)';
+let color2 = '#fff';
 
 changeParagraph(text);
 
 changeColorBackground(color);
+
+changeColorBackground2(color2);
