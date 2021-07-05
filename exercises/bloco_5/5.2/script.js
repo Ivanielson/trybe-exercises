4965f18b6059501a4e3921a75a9267945f1184bd
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-07-05 19:04:49 -0300

    Resolução do exercício_1.1 do bloco_5.2

---
+++ exercises/bloco_5/5.2/script.js
@@ -0,0 +1,13 @@
+// Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
+let h1Text = document.createElement('h1');
+let addText = h1Text.innerHTML = 'Exercício 5.2 - JavaScript DOM';
+document.body.appendChild(h1Text);
+
+// Adicione a tag div com a classe main-content como filho da tag body ;
+// Adicione a tag div com a classe center-content como filho da tag div criada no passo 2;
+// Adicione a tag p como filho do div criado no passo 3 e coloque algum texto;
+// Adicione a tag div com a classe left-content como filho da tag div criada no passo 2;
+// Adicione a tag div com a classe right-content como filho da tag div criada no passo 2;
+// Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do div criado no passo 5;
+// Adicione uma lista não ordenada com os valores de 1 a 10 por extenso como valores da lista. Essa lista deve ser filha do div criado no passo 6;
+// Adicione 3 tags h3 , todas sendo filhas do div criado no passo 2.
