77757fa94153740ba82b06903111ef8c428e5036
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-07-05 19:16:20 -0300

    Resolução do exercício_1.2 do bloco_5.2

--- exercises/bloco_5/5.2/script.js
+++ exercises/bloco_5/5.2/script.js
@@ -1,9 +1,13 @@
 // Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
-let h1Text = document.createElement('h1');
-let addText = h1Text.innerHTML = 'Exercício 5.2 - JavaScript DOM';
-document.body.appendChild(h1Text);
+let h1Prime = document.createElement('h1');
+let addText = h1Prime.innerText = 'Exercício 5.2 - JavaScript DOM';
+document.body.appendChild(h1Prime);
 
 // Adicione a tag div com a classe main-content como filho da tag body ;
+let divPrime = document.createElement('div');
+let divClass = divPrime.className = 'main-content';
+document.body.appendChild(divPrime);
+
 // Adicione a tag div com a classe center-content como filho da tag div criada no passo 2;
 // Adicione a tag p como filho do div criado no passo 3 e coloque algum texto;
 // Adicione a tag div com a classe left-content como filho da tag div criada no passo 2;
