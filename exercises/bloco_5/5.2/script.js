774f823545b2e5d29495d49096209208f2226e6f
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-07-06 00:29:17 -0300

    Resolução do exercício_1.5 do bloco_5.2

--- exercises/bloco_5/5.2/script.js
+++ exercises/bloco_5/5.2/script.js
@@ -19,6 +19,10 @@ let pText = newP.innerText = "Adicionando um parágrafo na tag DIV";
 document.querySelector('.center-content').appendChild(newP);
 
 // Adicione a tag div com a classe left-content como filho da tag div criada no passo 2;
+let divSecond = document.createElement('div');
+let divClassSecond = divSecond.className = 'left-content';
+document.querySelector('.main-content').appendChild(divSecond);
+
 // Adicione a tag div com a classe right-content como filho da tag div criada no passo 2;
 // Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do div criado no passo 5;
 // Adicione uma lista não ordenada com os valores de 1 a 10 por extenso como valores da lista. Essa lista deve ser filha do div criado no passo 6;
