fbc83413ca77d84b275e1da69657b020166b4550
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-07-06 00:39:39 -0300

    Resolução do exercício_1.7 do bloco_5.2

--- exercises/bloco_5/5.2/script.js
+++ exercises/bloco_5/5.2/script.js
@@ -1,33 +1,38 @@
-// Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
+// 1 - Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
 let h1Prime = document.createElement('h1');
 let addText = h1Prime.innerText = 'Exercício 5.2 - JavaScript DOM';
 document.body.appendChild(h1Prime);
 
-// Adicione a tag div com a classe main-content como filho da tag body ;
+// 2 - Adicione a tag div com a classe main-content como filho da tag body ;
 let divPrime = document.createElement('div');
 let divClass = divPrime.className = 'main-content';
 document.body.appendChild(divPrime);
 
-// Adicione a tag div com a classe center-content como filho da tag div criada no passo 2;
+// 3 - Adicione a tag div com a classe center-content como filho da tag div criada no passo 2;
 let divChild = document.createElement('div');
 let divClassChild = divChild.className = 'center-content';
 document.querySelector('.main-content').appendChild(divChild);
 
-// Adicione a tag p como filho do div criado no passo 3 e coloque algum texto;
+// 4 - Adicione a tag p como filho do div criado no passo 3 e coloque algum texto;
 let newP = document.createElement('p');
 let pText = newP.innerText = "Adicionando um parágrafo na tag DIV";
 document.querySelector('.center-content').appendChild(newP);
 
-// Adicione a tag div com a classe left-content como filho da tag div criada no passo 2;
+// 5 - Adicione a tag div com a classe left-content como filho da tag div criada no passo 2;
 let divSecond = document.createElement('div');
 let divClassSecond = divSecond.className = 'left-content';
 document.querySelector('.main-content').appendChild(divSecond);
 
-// Adicione a tag div com a classe right-content como filho da tag div criada no passo 2;
+// 6 - Adicione a tag div com a classe right-content como filho da tag div criada no passo 2;
 let divThird = document.createElement('div');
 let divClassThird = divThird.className = 'right-content';
 document.querySelector('.main-content').appendChild(divThird);
 
-// Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do div criado no passo 5;
-// Adicione uma lista não ordenada com os valores de 1 a 10 por extenso como valores da lista. Essa lista deve ser filha do div criado no passo 6;
-// Adicione 3 tags h3 , todas sendo filhas do div criado no passo 2.
+// 7 - Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do div criado no passo 5;
+let imgPrime = document.createElement('img');
+let imgClassPrime = imgPrime.className = 'small-image';
+let imgSrcPrime = imgPrime.src = 'https://picsum.photos/200';
+document.querySelector('.left-content').appendChild(imgPrime);
+
+// 8 - Adicione uma lista não ordenada com os valores de 1 a 10 por extenso como valores da lista. Essa lista deve ser filha do div criado no passo 6;
+// 9 - Adicione 3 tags h3 , todas sendo filhas do div criado no passo 2.
