06cf8180c75b3d2d77573e40445ea009731b7832
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-07-14 19:27:45 -0300

    Resolução do exercício_1.2 do bloco_6.1

--- exercises/bloco_6/6.1/js/script.js
+++ exercises/bloco_6/6.1/js/script.js
@@ -1,6 +1,6 @@
 const selectEstado = document.querySelector('#select-estado');
+const inputDate = document.querySelector('#input-date-start');
 const estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
-
 const siglas = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
 
 for (let index = 0; index < estados.length; index += 1) {
@@ -10,3 +10,9 @@ for (let index = 0; index < estados.length; index += 1) {
   selectEstado.appendChild(newOption);
 }
 
+function verificaData() {
+  const dateFormat = /^[0-9]{2}\/^[0-9]{2\/[0-9]{4}$/;
+  if (!dateFormat.test(inputDate.value)) {
+    alert('Preencher data no formato dd/mm/aaaa');
+  }
+}
