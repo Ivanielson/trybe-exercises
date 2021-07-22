ede751d769c93801e29e13c9f5fe11b9f09baeba
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-07-22 18:52:38 -0300

    Resolução do exercício_1.1 do bloco_7.1

---
+++ exercises/bloco_7/7.1/script.js
@@ -0,0 +1,11 @@
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    console.log(`${ifScope} ótimo, fui utilizada no escopo !`);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+
+testingScope(true);
