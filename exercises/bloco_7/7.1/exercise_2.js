df2a78b401ecb3d44bd8a794762188271221802c
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-07-23 14:48:03 -0300

    Resolução do exercício_2.2 do bloco_7.1

--- exercises/bloco_7/7.1/exercise_2.js
+++ exercises/bloco_7/7.1/exercise_2.js
@@ -1,11 +1,26 @@
-const n = 5;
+// const n = 5;
 
-const fatorial = (n) => {
-  let result = 1;
-  for (let index = 1; index < n; index += 1) {
-    result += result * index;
+// const fatorial = (n) => {
+//   let result = 1;
+//   for (let index = 1; index < n; index += 1) {
+//     result += result * index;
+//   }
+//   return result;
+// }
+
+// console.log(fatorial(n));
+
+const countWordSize = (frase) => {
+  const words = frase.split(' ');
+  let count = 0;
+  let wordSize;
+  for (const value of words) {
+    if (value.length > count) {
+      count = value.length;
+      wordSize = value;
+    }
   }
-  return result;
+  return wordSize;
 }
 
-console.log(fatorial(n));
+console.log(countWordSize('Antônio foi no banheiro e não sabemos o que aconteceu'));
