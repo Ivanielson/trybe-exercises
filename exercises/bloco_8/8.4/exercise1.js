8279ae764026eb698bc939556287014da0580abe
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-08-02 10:41:40 -0300

    Adiciona referêcia da função concat no arquivo

--- exercises/bloco_8/8.4/exercise1.js
+++ exercises/bloco_8/8.4/exercise1.js
@@ -8,6 +8,7 @@ const arrays = [
 
 function flatten() {
   // escreva seu código aqui
+  // Referência função concat() -> Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/concat#example_concatenating_two_arrays
   return arrays.reduce((result, value) => result.concat(value));
 }
 
