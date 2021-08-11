1ea8a1a7251ef5fe24cfe373dc9cbcc11cac5ce6
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-08-11 18:40:37 -0300

    Resolução do exercício_1 do bloco_9.2

--- exercises/bloco_9/9.2/apiScript.js
+++ exercises/bloco_9/9.2/apiScript.js
@@ -1,8 +1,22 @@
 // apiScript.js
 const API_URL = 'https://icanhazdadjoke.com/';
 
+const append = (data) => {
+  const getParagraph = document.querySelector('#joke');
+  const newParagraph = document.createElement('p');
+  newParagraph.innerText = data;
+  getParagraph.appendChild(newParagraph);
+}
+
 const fetchJoke = () => {
   // Adicionar lógica aqui!
+  const myObject = {
+    method: 'GET',
+    headers: {'Accept': 'application/json'}
+  };
+  fetch(API_URL, myObject)
+    .then((response) => response.json())
+    .then(({ joke }) => append(joke));
 };
 
 window.onload = () => fetchJoke();
