11d4854d06e1476c2befce924420dd7c37cf27d9
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-08-17 15:15:39 -0300

    Resolução do exercício_1.2 do bloco_10.1

---
+++ exercises/bloco_10/10.1/exercise1-2.test.js
@@ -0,0 +1,16 @@
+const myRemove = require('./exercise1-2');
+
+describe('Teste da função myRemove - Exercício 2', () => {
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o [1, 2, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toContain(3);
+  });
+
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+  
+});
