afc863e7b63cd4cc404040836c1a5f1bb73aa075
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-08-17 14:58:15 -0300

    Resolução do exercício_1.1 do bloco_10.1

---
+++ exercises/bloco_10/10.1/exercise1-1.test.js
@@ -0,0 +1,19 @@
+const sum = require('./exercise1-1');
+
+describe('Testa função sum()', () => {
+  it('A função recebe sum(4, 5) e retorna o valor 9', () => {
+    expect(9).toBe(sum(4, 5));
+  });
+
+  it('Teste se o retorno de sum(0, 0) é 0', () => {
+    expect(0).toBe(sum(0, 0));
+  });
+
+  it('Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)', () => {
+    expect(() => {sum(4, "5")}).toThrow();
+  });
+
+  it('Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
+    expect(() => {sum(4, "5")}).toThrowError(new Error('parameters must be numbers'));
+  });
+});
