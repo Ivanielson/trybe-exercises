92bddc1fb0b5b311e673d2b58437e4831eafdab7
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-08-19 23:34:54 -0300

    Divisão de arquivos para os exercícios

--- exercises/bloco_10/10.3/test/generationNumber.spec.js
+++ exercises/bloco_10/10.3/test/generationNumber.spec.js
@@ -1,7 +1,5 @@
 const generationNumber = require('../src/generationNumber');
 
-jest.mock('../src/generationNumber');
-
 describe('Quando a função generationNumber é chamada', () => {
   it('Retorna um número aleatório entre 0 e 100', () => {
 
@@ -11,17 +9,5 @@ describe('Quando a função generationNumber é chamada', () => {
     expect(generationNumber.generationNumber).toHaveBeenCalled();
     expect(generationNumber.generationNumber).toHaveBeenCalledTimes(1);
     expect(generationNumber.generationNumber()).toBe(10);
-  });
-});
-
-describe('Quando a função generationNumber é chamada', () => {
-  it('Recebe dois parametros e retorna a divisão do primeiro parametro pelo segundo ', () => {
-
-    generationNumber.generationNumber.mockImplementation((a , b) => a / b);
-
-    generationNumber.generationNumber(10, 2);
-    expect(generationNumber.generationNumber).toHaveBeenCalled();
-    expect(generationNumber.generationNumber).toHaveBeenCalledTimes(3);
-    expect(generationNumber.generationNumber(20, 4)).toBe(5);
   });
 });
