ae99eb62bb258dc07480b866ef37af1af39feddf
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-08-19 23:29:30 -0300

    Resolução do exercício_2 do bloco_10.3

--- exercises/bloco_10/10.3/test/generationNumber.spec.js
+++ exercises/bloco_10/10.3/test/generationNumber.spec.js
@@ -1,12 +1,27 @@
 const generationNumber = require('../src/generationNumber');
 
+jest.mock('../src/generationNumber');
+
 describe('Quando a função generationNumber é chamada', () => {
   it('Retorna um número aleatório entre 0 e 100', () => {
+
     generationNumber.generationNumber = jest.fn().mockReturnValue(10);
 
     generationNumber.generationNumber();
     expect(generationNumber.generationNumber).toHaveBeenCalled();
     expect(generationNumber.generationNumber).toHaveBeenCalledTimes(1);
     expect(generationNumber.generationNumber()).toBe(10);
+  });
+});
+
+describe('Quando a função generationNumber é chamada', () => {
+  it('Recebe dois parametros e retorna a divisão do primeiro parametro pelo segundo ', () => {
+
+    generationNumber.generationNumber.mockImplementation((a , b) => a / b);
+
+    generationNumber.generationNumber(10, 2);
+    expect(generationNumber.generationNumber).toHaveBeenCalled();
+    expect(generationNumber.generationNumber).toHaveBeenCalledTimes(3);
+    expect(generationNumber.generationNumber(20, 4)).toBe(5);
   });
 });
