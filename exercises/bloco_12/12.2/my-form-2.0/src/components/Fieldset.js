b5e8cb63c49dd428087c70ef69be88a6de93c31b
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-09-03 19:41:06 -0300

    Pojeto iniciado

---
+++ exercises/bloco_12/12.2/my-form-2.0/src/components/Fieldset.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Fieldset extends React.Component {
+  render() {
+    return (
+      <fieldset>
+        <legend>Dados Pessoais</legend>
+      </fieldset>
+    );
+  }
+}
+
+export default Fieldset;
