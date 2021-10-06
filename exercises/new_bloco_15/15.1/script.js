6bc33b587aaf52ecff6668e8925891c44cc59691
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-10-06 14:46:49 -0300

    impl: store da aplicação

---
+++ exercises/new_bloco_15/15.1/script.js
@@ -0,0 +1,10 @@
+const ESTADO_INICIAL = {
+  colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
+  index: 0,
+};
+
+const reducer = (state) => {
+  return state;
+}
+
+const store = Redux.createStore(reducer);
