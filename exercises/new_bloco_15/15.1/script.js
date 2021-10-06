42fd7e389f616f29678f8f47e5ec040f6781b923
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-10-06 18:07:00 -0300

    impl: switch do reduncer

--- exercises/new_bloco_15/15.1/script.js
+++ exercises/new_bloco_15/15.1/script.js
@@ -1,10 +1,19 @@
+// const query = (element) => document.querySelector(element);
+
 const ESTADO_INICIAL = {
   colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
   index: 0,
 };
 
+const nextColor = 'NEXT_COLOR';
+const previousColor = 'PREVIOUS_COLOR';
+
 const reducer = (state = ESTADO_INICIAL, action) => {
   switch (action.type) {
+    case nextColor:
+      if (state.index < state.colors.length) return state.index + 1;
+    case previousColor:
+      if (state.index > 0) return state.index -1;
     default:
       return state;
   }
