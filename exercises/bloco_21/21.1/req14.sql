5a1f6d23fec6a9e35aa1e51bdd4443d4c53b43a6
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-12-14 15:40:49 -0300

    Resolução do exercício do bloco_21.1 Questão 14

---
+++ exercises/bloco_21/21.1/req14.sql
@@ -0,0 +1,6 @@
+-- 14. Escreva uma query que exiba as seguintes informações de cada funcionário:
+-- id , primeiro nome e dia do mês no qual foi contratado (exiba somente o dia).
+SELECT 
+    EMPLOYEE_ID, FIRST_NAME, DAY(HIRE_DATE)
+FROM
+    hr.employees;
