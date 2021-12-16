a6974fa2ee7cae9aa5ec2f86b1ec4850882d8e9b
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-12-16 10:31:39 -0300

    Resolução de exercício do bloco_21.2 Questão 1

---
+++ exercises/bloco_21/21.2/req1.sql
@@ -0,0 +1,8 @@
+-- 1.  Utilizando o INNER JOIN , encontre as vendas nacionais ( domestic_sales )
+-- e internacionais ( international_sales ) de cada filme.
+SELECT 
+    movies.title, Box.domestic_sales, Box.international_sales
+FROM
+    Pixar.Movies AS `movies`
+        INNER JOIN
+    Pixar.BoxOffice AS `Box` ON movies.id = Box.movie_id;
