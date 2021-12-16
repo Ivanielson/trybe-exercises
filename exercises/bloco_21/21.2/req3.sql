378dec1e57849f999e4416273e81c203bcbc8a98
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-12-16 10:46:48 -0300

    Resolução de exercício do bloco_21.2 Questão 3

---
+++ exercises/bloco_21/21.2/req3.sql
@@ -0,0 +1,9 @@
+-- Utilizando o INNER JOIN , faça uma busca que retorne os filmes e sua avaliação
+-- ( rating ) em ordem decrescente.
+SELECT 
+    movies.title, box.rating
+FROM
+    Pixar.Movies AS `movies`
+        INNER JOIN
+    Pixar.BoxOffice AS `box` ON movies.id = box.movie_id
+ORDER BY box.rating DESC;
