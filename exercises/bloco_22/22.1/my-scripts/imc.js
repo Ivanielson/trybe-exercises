dd15c171b5ffa0967332a3bac617039279f02961
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-01-09 00:13:32 -0300

    impl: Entrada de peso e altura de forma dinâmica

--- exercises/bloco_22/22.1/my-scripts/imc.js
+++ exercises/bloco_22/22.1/my-scripts/imc.js
@@ -1,3 +1,8 @@
-const imc = (height, weight) => Math.fround(weight / Math.pow(height, 2)).toFixed(2);
+const readline = require('readline-sync');
 
-console.log(imc(1.73, 94));
+const weight = readline.questionFloat('Qual seu peso? ');
+const height = readline.questionFloat('Qual sua altura? ');
+
+const imc = (weight, height) => Math.fround(weight / Math.pow(height, 2)).toFixed(2);
+
+console.log(imc(weight, height));
