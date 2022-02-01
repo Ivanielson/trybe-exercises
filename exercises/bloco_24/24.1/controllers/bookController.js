8277b565f0bc58775dc538d1fc6c8a44757993ea
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-01-31 21:19:40 -0300

    Impl: 'GET /books/:id' e 'POST /books' no bookController

--- exercises/bloco_24/24.1/controllers/bookController.js
+++ exercises/bloco_24/24.1/controllers/bookController.js
@@ -12,4 +12,27 @@ router.get('/', async (_req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const bookId = await Book.findByPk(id);
+
+    return res.status(200).json(bookId);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+router.post('/', async (req,res) => {
+  try {
+    const { title, author, pageQuantity } = req.body;
+    const newBook = await Book.create({ title, author, pageQuantity });
+    res.status(201).json(newBook);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
 module.exports = router;
