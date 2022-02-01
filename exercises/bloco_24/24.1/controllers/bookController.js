6192aa238df6815941bfb5c1ffe27d0662ef9ac7
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-02-01 08:55:08 -0300

    Impl: 'PUT /books/:id' no bookController

--- exercises/bloco_24/24.1/controllers/bookController.js
+++ exercises/bloco_24/24.1/controllers/bookController.js
@@ -8,7 +8,9 @@ router.get('/', async (_req, res) => {
     return res.status(200).json(bookAll);
   } catch (error) {
     console.log(error.message);
-    res.status(500).json({ message: 'Algo deu errado' });
+    res.status(500).json({
+      message: 'Algo deu errado'
+    });
   }
 });
 
@@ -20,18 +22,49 @@ router.get('/:id', async (req, res) => {
     return res.status(200).json(bookId);
   } catch (error) {
     console.log(error.message);
-    res.status(500).json({ message: 'Algo deu errado' });
+    res.status(500).json({
+      message: 'Algo deu errado'
+    });
   }
 });
 
-router.post('/', async (req,res) => {
+router.post('/', async (req, res) => {
   try {
     const { title, author, pageQuantity } = req.body;
-    const newBook = await Book.create({ title, author, pageQuantity });
-    res.status(201).json(newBook);
+    const newBook = await Book.create({
+      title, author, pageQuantity,
+    });
+
+    return res.status(201).json(newBook);
   } catch (error) {
     console.log(error.message);
-    res.status(500).json({ message: 'Algo deu errado' });
+    res.status(500).json({
+      message: 'Algo deu errado'
+    });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const { title, author, pageQuantity } = req.body;
+    const { id } = req.params;
+    const [bookUpdate] = await Book.update(
+      { title, author, pageQuantity },
+      { where: { id } },
+    );
+
+    if (!bookUpdate) return res.status(404).json({
+      message: 'Book não encontrado!'
+    });
+
+    return res.status(200).json({
+      message: 'Book atualizado com sucesso!',
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      message: 'Algo deu errado',
+    });
   }
 });
 
