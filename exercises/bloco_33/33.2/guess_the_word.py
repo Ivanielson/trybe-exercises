9603fe4ac316f8180647182b918c33ff48492c0a
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-05-29 18:07:39 -0300

    :sparkles: feat: Exercício 2

---
+++ exercises/bloco_33/33.2/guess_the_word.py
@@ -0,0 +1,21 @@
+import random
+
+
+words = [
+    "carro", "passáro", "javascript", "python", "react", "desenvolvedor",
+]
+
+
+def guess_the_word(list_words):
+    word_drawn = random.choice(list_words)
+    scrambled_word = "".join(random.sample(word_drawn, len(word_drawn)))
+    print(scrambled_word)
+    palpite = input("Digite seu palpite, qual a palavra? ")
+    if palpite == word_drawn:
+        print(f"Parabéns! você ganhou, a palavra era: {word_drawn}")
+    else:
+        print(f"Que pena! você errou, a plavra correta é: {word_drawn}")
+
+
+if __name__ == "__main__":
+    guess_the_word(words)
