dbe002b7dbf4e646c077b2c2f3a2a42fb9011c43
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-06-06 20:35:20 -0300

    :sparkles: feat: characters interface

--- exercises/bloco_34/34.2/star_wars.py
+++ exercises/bloco_34/34.2/star_wars.py
@@ -1,4 +1,15 @@
-class Soldier:
+from abc import ABC, abstractmethod
+
+
+class Character(ABC):
+    @classmethod
+    @abstractmethod
+    def attack(self):
+        raise NotImplementedError
+
+
+class Soldier(Character):
+    @classmethod
     def __init__(self, level):
         self.level = level
 
@@ -6,16 +17,20 @@ class Soldier:
         return self.level * 1
 
 
-class Jedi:
+class Jedi(Character):
+    @classmethod
     def __init__(self, level):
         self.level = level
 
     def attackWithSaber(self):
         return self.level * 100
 
+    def attack(self):
+        return self.level * 10
+
 
 class StarWarsGame:
-    def __init__(self, character):
+    def __init__(self, character: Character):
         self.character = character
 
     def fight_enemy(self):
